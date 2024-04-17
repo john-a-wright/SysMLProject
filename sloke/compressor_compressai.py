ed062ddaac0295f6aeef7a2b4fb9044c239d5cc7
Sloke Shrestha <slokeshrestha131@gmail.com>
2024-04-17 16:48:00 -0500

    training seems to be better now

--- sloke/compressor_compressai.py
+++ sloke/compressor_compressai.py
@@ -4,6 +4,11 @@ import torch.nn as nn
 from compressai.entropy_models import EntropyBottleneck
 import torch
 
+class ChannelToSpatial(nn.Module):
+    def forward(self, x):
+        batch_size, channels, spatial_dim = x.size()
+        return x.view(batch_size, spatial_dim, channels)
+
 
 class Network(CompressionModel):
     def __init__(self, compressed_d = 64, uncompressed_d = 128):
@@ -11,45 +16,52 @@ class Network(CompressionModel):
         compressed_d = compressed dimension of the embedding
         """
         super().__init__()
-        self.entropy_bottleneck = EntropyBottleneck(compressed_d)
+        self.entropy_bottleneck = EntropyBottleneck(1)
         self.encoder = nn.Sequential(
             nn.Conv1d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv1d(32, compressed_d, kernel_size=3, stride=3, padding=1),
+            nn.Conv1d(32, compressed_d, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv1d(compressed_d, compressed_d, kernel_size=3, stride=3, padding=1),
+            nn.Conv1d(compressed_d, compressed_d, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=3, stride=2),
             nn.ReLU(),
-            nn.Conv1d(compressed_d, compressed_d, kernel_size=3, stride=3, padding=1),
+            nn.Conv1d(compressed_d, compressed_d, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AdaptiveAvgPool1d((1,)),
-            nn.Linear(compressed_d, compressed_d),
+            nn.Conv1d(compressed_d, compressed_d//2, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(compressed_d, compressed_d)
+            nn.Conv1d(compressed_d//2, 1, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.LazyLinear(compressed_d),
+            nn.ReLU(),
+            nn.LazyLinear(compressed_d)
         )
 
         self.decoder = nn.Sequential(
-            nn.ConvTranspose1d(1, 32, kernel_size=3, stride=3, padding=1),
+            nn.ConvTranspose1d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose1d(32, compressed_d, kernel_size=3, stride=3, padding=1),
+            nn.ConvTranspose1d(32, compressed_d, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose1d(compressed_d, uncompressed_d, kernel_size=3, stride=3, padding=1),
+            nn.ConvTranspose1d(compressed_d, uncompressed_d, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.ConvTranspose1d(uncompressed_d, uncompressed_d, kernel_size=3, stride=3, padding=1),
+            nn.ConvTranspose1d(uncompressed_d, uncompressed_d, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.AdaptiveAvgPool1d((1,)),
-            nn.Linear(uncompressed_d, uncompressed_d),
+            nn.ConvTranspose1d(uncompressed_d, uncompressed_d, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Linear(uncompressed_d, uncompressed_d)
+            nn.ConvTranspose1d(uncompressed_d, uncompressed_d//2, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose1d(uncompressed_d//2, 1, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.LazyLinear(uncompressed_d),
+            nn.ReLU(),
+            nn.LazyLinear(uncompressed_d)
         )
     
     def forward(self, x):
         y = self.encoder(x)
+        
         y_hat, y_likelihoods = self.entropy_bottleneck(y)
 
-        y_hat = torch.permute(y_hat, (0, 2, 1))
         x_hat = self.decoder(y_hat)
-        x_hat = torch.permute(x_hat, (0, 2, 1))
 
         return x_hat, y_likelihoods
